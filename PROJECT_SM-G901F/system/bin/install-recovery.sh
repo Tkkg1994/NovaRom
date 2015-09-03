a4fc18a015a83850f6aa74cd39f71c01967ac26b
Tkkg1994 <luca.grifo@outlook.com>
2015-09-03 18:54:52 +0200

    OH4 base, already deodex, rooted, busybox support and knox removed

--- PROJECT_SM-G901F/system/bin/install-recovery.sh
+++ PROJECT_SM-G901F/system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:12775424:b6830ac2e08e2064c0443969614e5904574aa3df; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:11476992:527d6be556ad39cf48b3f7332d872803cd13e32c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery b6830ac2e08e2064c0443969614e5904574aa3df 12775424 527d6be556ad39cf48b3f7332d872803cd13e32c:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:12775424:668234dcfe61897b0fd5ea9ecaeb8e530bc7fd16; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:11476992:06d0686c0aac3a72752db3782d0c0801e86cc0ea EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 668234dcfe61897b0fd5ea9ecaeb8e530bc7fd16 12775424 06d0686c0aac3a72752db3782d0c0801e86cc0ea:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
