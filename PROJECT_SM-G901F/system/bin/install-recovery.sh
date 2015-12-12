44456ef37d2786f76425332278e13d76b4d0f562
Tkkg1994 <luca.grifo@outlook.com>
2015-12-12 18:49:05 +0100

    LRX22G.G901FXXU1BOK2 already rooted and deodexed

--- PROJECT_SM-G901F/system/bin/install-recovery.sh
+++ PROJECT_SM-G901F/system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:12775424:668234dcfe61897b0fd5ea9ecaeb8e530bc7fd16; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:11476992:06d0686c0aac3a72752db3782d0c0801e86cc0ea EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 668234dcfe61897b0fd5ea9ecaeb8e530bc7fd16 12775424 06d0686c0aac3a72752db3782d0c0801e86cc0ea:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:12775424:fbb106ddfabd7695cd0948323bb249e99109aeb1; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:11476992:dfe1a3b92033806fa24022f7cf7142aa8485bbe5 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery fbb106ddfabd7695cd0948323bb249e99109aeb1 12775424 dfe1a3b92033806fa24022f7cf7142aa8485bbe5:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
